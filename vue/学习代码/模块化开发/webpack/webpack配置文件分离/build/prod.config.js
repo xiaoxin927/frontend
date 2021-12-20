8b2e2a710c862c56a42df632ac957735bef8cce5
小金刚 <1731827157@qq.com>
2021-12-20 23:04:59 +0800

    2021 1220

---
+++ vue/学习代码/模块化开发/webpack/webpack配置文件分离/build/prod.config.js
@@ -0,0 +1,7 @@
+const UglifyjsWebpackPlugin = require("uglifyjs-webpack-plugin");
+const webpackMerge = require("webpack-merge");
+const baseConfig = require("./base.config");
+
+module.exports = webpackMerge(baseConfig, {
+  plugins: [new UglifyjsWebpackPlugin()],
+});
---
+++ vue/学习代码/模块化开发/webpack/webpack配置文件分离/src/main.js
@@ -0,0 +1,34 @@
+//使用commonjs的模块化规范
+const { add, mul } = require("./js/mathUtil.js");
+
+console.log(add(20, 30));
+console.log(mul(20, 30));
+
+// 使用ES6的模块化规范
+
+import { name, age, height } from "./js/info.js";
+
+console.log(name);
+console.log(age);
+console.log(height);
+
+//引入css文件
+require("./css/normal.css");
+// 引入less文件也是这样
+
+//使用vue进行开发
+
+import Vue from "vue";
+// import App from "./vue/app.js";
+import App from "./vue/App.vue";
+
+new Vue({
+  el: "#app",
+  // 这个template会直接将挂载index.html里面的id=app的div全部替换
+  template: "<App/>",
+  components: {
+    App,
+  },
+});
+
+document.writeln("<span>xiaojingang</span>");
