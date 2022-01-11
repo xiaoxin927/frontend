803831c9217cae67964d67fe7f8a3a07b78dca7c
小金刚 <1731827157@qq.com>
2022-01-11 23:27:50 +0800

    2022 01 11

---
+++ vue/学习代码/模块化开发/tabbar/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <tab-bar>
+      
+    </tab-bar>
+
+  </div>
+</template>
+
+<script>
+
+import TabBar from './components/tabbar/TabBar'
+
+export default {
+  name: 'App',
+  components: {
+    TabBar
+  }
+}
+</script>
+
+<style>
+  @import './assets/css/base';
+
+ 
+</style>
